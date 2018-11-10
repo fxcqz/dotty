72492045f84b2794b35a696768e04acb864edb8a
matt <mhrawcliffe@gmail.com>
2018-11-10 19:26:31 +0000

    support multiple plugin

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.json : JSONValue;
 import std.stdio : writeln;
+import std.typecons : tuple;
 
 import config : Config, readConfig;
 import matrix : Matrix;
@@ -8,9 +9,10 @@ import message : Message;
 import plugins.core : Core;
 import plugins.utils : callCommands, callNoPrompt;
 
+
 void run(ref Matrix connection) {
     immutable string symbol = connection.getSymbol();
-    Core corePlugin = new Core();
+    auto plugins = tuple(new Core());
 
     connection.login();
     connection.join();
@@ -27,21 +29,26 @@ void run(ref Matrix connection) {
         }
 
         connection.markRead(messages[$ - 1]);
-        foreach (message; messages) {
+        foreach (ref message; messages) {
             if (message.sender == connection.getUserID()) {
                 // ignore our own messages
                 continue;
             }
+
             // TODO: pass messageCount to sendMessage, if it's more than
             // one, quote the original message in the reply
-            string response = callCommands(symbol, corePlugin, message);
-            if (response.length > 0) {
-                connection.sendMessage(response);
-            }
-            // run generic command
-            response = callNoPrompt(corePlugin, message);
-            if (response.length > 0) {
-                connection.sendMessage(response);
+
+            foreach(ref plugin; plugins) {
+                string response = callCommands(symbol, plugin, message);
+                if (response.length > 0) {
+                    connection.sendMessage(response);
+                }
+
+                // run generic command
+                response = callNoPrompt(plugin, message);
+                if (response.length > 0) {
+                    connection.sendMessage(response);
+                }
             }
         }
     }
