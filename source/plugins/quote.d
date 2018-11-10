cb4c3cc3858eabf7cef3668f1e55b16c1cc3effd
matt <mhrawcliffe@gmail.com>
2018-11-10 20:19:55 +0000

    add a quote get command (not random yet)

--- source/plugins/quote.d
+++ source/plugins/quote.d
@@ -1,10 +1,14 @@
 module plugins.quote;
 
 import std.array : join, split;
+import std.format : format;
 
-import d2sqlite3 : Database, Statement;
+import d2sqlite3 : Database, Row, Statement;
 import message : Message;
 import plugins.utils : command;
+
+// Useful doc at:
+// http://biozic.github.io/d2sqlite3/d2sqlite3.html
 
 class Quote {
     @command
@@ -14,12 +18,26 @@ class Quote {
         string quote = tokens[1 .. $].join(" ");
 
         Statement query = db.prepare(
-            "INSERT INTO quote (category, quote) VALUES (:category, :quote)"
+            "INSERT INTO quotes (category, quote) VALUES (:category, :quote)"
         );
         query.bind(":category", category);
         query.bind(":quote", quote);
         query.execute();
         query.reset();
+        return "";
+    }
+
+    @command
+    string qget(ref Database db, const Message message) {
+        string category = message.text.split(" ")[0];
+        auto results = db.execute("SELECT quote FROM quotes WHERE category = '%s'".format(
+            category
+        ));
+
+        // TODO make it random
+        foreach(Row row; results) {
+            return row["quote"].as!string;
+        }
         return "";
     }
 }
